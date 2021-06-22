8822b1d15a835a354a9e83c336938587593dd8a9
Bao phung <b.giaphung@gmail.com>
2021-06-22 18:17:42 -0500

    data update and removing unused icons

--- src/gatsby-theme-mate/icons.js
+++ src/gatsby-theme-mate/icons.js
@@ -1,19 +1,8 @@
-import {
-  faMedium,
-  faGithub,
-  faLinkedin,
-} from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import {
   faGlobe,
   faEnvelope,
   faCoffee,
 } from "@fortawesome/free-solid-svg-icons";
 
-export const ICONS = [
-  faMedium,
-  faGithub,
-  faGlobe,
-  faEnvelope,
-  faCoffee,
-  faLinkedin,
-];
+export const ICONS = [faGithub, faGlobe, faEnvelope, faCoffee, faLinkedin];
