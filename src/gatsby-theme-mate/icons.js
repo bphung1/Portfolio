adf57e9b4e6d0b9a33545f5413172993bec4e660
Bao phung <b.giaphung@gmail.com>
2021-06-22 13:21:52 -0500

    adding portfolio

---
+++ src/gatsby-theme-mate/icons.js
@@ -0,0 +1,19 @@
+import {
+  faMedium,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faGlobe,
+  faEnvelope,
+  faCoffee,
+} from "@fortawesome/free-solid-svg-icons";
+
+export const ICONS = [
+  faMedium,
+  faGithub,
+  faGlobe,
+  faEnvelope,
+  faCoffee,
+  faLinkedin,
+];
